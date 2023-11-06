83bc4c14056c8919842321d56f7ae2644369287c
ibiokjr <uwemibiok1999@gmail.com>
2023-11-06 14:08:25 +0100

    update changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// import Navbar from './Components/Navbar'
+// import Card from './Components/Card'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import './App.css'
+import ButtonCounter from './Components/ButtonCounter'
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ButtonCounter/>
+      </div>
+    )
+  }
+}
---
+++ src/Components/Card.jsx
@@ -0,0 +1,17 @@
+// import React, { Component } from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// export default class Card extends Component {
+//   render() {
+//     return (
+//       <div className='card'>
+//     <h5 className="card-text">Increment Using React State with Class Component</h5>
+//     <p className="card-text">This project uses React state with Class Component to increment counts. If the button is clicked up to two times, Cart Item changes to Cart Items and the<br>
+//     </br>Page Title as well updates.</p>
+//     <h5 className='card-text'> Cart Item: 0 </h5>
+//     <h5 className='card-text'>Click the button To Change State</h5>
+//   </div>
+      
+//     )
+//   }
+// }
